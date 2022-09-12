46edfa0e3c0904ea84dbf8b9757c425f54245ad8
jm_b <jmbanchero@gmail.com>
2022-09-12 13:28:09 +0100

    add

--- src/FirstApp.jsx
+++ src/FirstApp.jsx
@@ -1,18 +1,20 @@
 import React from 'react'
 
+
 const msm = {
 	title: 'Title' ,
 	mes: 'Hola', 
 }; 
-//no renderiza lo que esta fuera del
+// no renderiza lo que esta fuera del
 // si no hay dependencia debe ir fuera el Functional Component
 const getSaludo = (nombre) => `Saluda a  ${nombre}`;
 
-export const FirstApp = () => {
-
+export const FirstApp = ({title, subtitle}) => {
+	// console.log(title);
 	return (
 		<>
-		<h1>FirstApp</h1>
+		<h1>FirstApp {title}</h1>
+		<p> {subtitle + 1 }</p>
 		<p>{getSaludo('pepe')}</p>
 		{/* <p>{ JSON.stringify(msm) }</p> */}
 
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import {FirstApp}  from './FirstApp'
 import './index.css'
+import './styles.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <FirstApp />
+    <FirstApp title="hola tu" subtitle={1} />
   </React.StrictMode>
 )
