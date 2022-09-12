e700e4558f33812399cbd87ea58c4ffbea3a4b7e
jm_b <jmbanchero@gmail.com>
2022-09-12 14:07:01 +0100

    event

--- src/CouterApp.jsx
+++ src/CouterApp.jsx
@@ -2,10 +2,20 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 export const CouterApp = ({value}) => {
+
+	const addValue = (event, newVaLUE) => {
+		console.log(event)
+		console.log(newVaLUE)
+	}
+
 	return (
 		<>
 		<div>CouterApp</div>
-		<h2>{ value}</h2>
+		<h2>{ value }</h2>
+		<button onClick={ (event) => addValue(event, 'HOLA TU')}>
+			+1
+		</button>
+
 		</>
 	)
 }
